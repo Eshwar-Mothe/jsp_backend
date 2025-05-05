03ce0cdf2c25ddd155397b325d72a7656f5b8804
Eshwar-Mothe <mothe.eshwar.em@gmail.com>
2025-05-06 00:46:14 +0530

    updated files

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const axios = require('axios');
+const sendMail = require('./mailService');
 require('dotenv').config();
 
 const app = express();
@@ -12,13 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get('/videoGallery', async (req, res) => {
     try {
-        // const apiKey = process.env.REACT_APP_API_KEY;
-        const apiKey = 'AIzaSyBZcflCeB_aoRC9warXZTyA5OcFTa9C_JM';
+        const apiKey = 'AIzaSyD10DfyGKnEuR8gdeHyv1-Xh85f6oyn8_8';
         const response = await axios.get('https://www.googleapis.com/youtube/v3/search', {
             params: {
                 key: apiKey,
                 part: 'snippet',
-                maxResults: 120,
+                maxResults: 12,
                 order: 'date',
                 channelId: 'UCrKevLQTcgUp2kZ-WE0pWZQ',
                 type: 'video'
@@ -32,6 +32,25 @@ app.get('/videoGallery', async (req, res) => {
     }
 });
 
+
+app.post("/sendMail", async (req, res) => {
+    console.log("Received Data:", req.body);
+    try {
+        const { to, subject, text } = req.body;
+
+        const payload = { to, subject, text };
+
+        console.log("Sent:", payload);
+        const response = await sendMail(payload);
+        res.status(200).json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Failed to send email." });
+    }
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+
---
+++ mailService.js
@@ -0,0 +1,38 @@
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
+
+const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST || "smtp.gmail.com",
+    port: process.env.EMAIL_PORT || 587,
+    secure: process.env.EMAIL_SECURE === "true", 
+    auth: {
+        user: 'rupeeinsight@gmail.com',
+        pass: 'pwzr ddeq hmht noqz',
+    },
+});
+
+const sendMail = async ({ to, subject, text }) => {
+    try {
+        if (!to || !subject || !text) {
+            throw new Error("Missing required email parameters: 'to', 'subject', or 'text'");
+        }
+
+        console.log(`Sending email to: ${to}\nSubject: ${subject}\nText: ${text}`);
+
+        const info = await transporter.sendMail({
+            from: `"No Reply" <${process.env.EMAIL_USER}>`,
+            to,
+            subject,
+            text,
+        });
+
+        console.log("Email sent successfully: ", info.messageId);
+        return { success: true, messageId: info.messageId, message: "OTP Sent Successfully", status: 200 };
+    } catch (error) {
+        console.error("Error sending email: ", error);
+        return { success: false, error: error.message, status: 500 };
+    }
+};
+
+module.exports = sendMail;
